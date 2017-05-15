73775a19ecb767097cad88c773ab002ebaf5d929
Oscar Arreola <taiyo472004@yahoo.com>
2017-05-15 06:01:14 +0000

    Added check_packet() to both client and serve

--- client.c
+++ client.c
@@ -8,21 +8,141 @@
 #include<sys/socket.h>
  
 #define SERVER "127.0.0.1"
-#define BUFLEN 512  //Max length of buffer
+#define BUFLEN 264  //Max length of buffer was 512
 #define PORT 8888   //The port on which to send data
- 
+
+// globals
+int segment_number = 0x00;
+
 void die(char *s)
 {
     perror(s);
     exit(1);
 }
- 
+
+/*
+ * Check packet structure
+ *      start id:       0xFFFF
+ *      client id:      0x0a    // for example
+ *      Pkt types:
+ *          DATA:       0xFFF1
+ *          ACK:        0xFFF2
+ *          REJECT:     0xFFF3
+ *      For DATA Type:
+ *          sent segment:   0x01    // ex: for 1st segment
+ *          data length:    0xFF    // for 255 data bytes
+ *          payload:        0xFF bytes // using "da" for data
+ *      For ACK Type:
+ *          rcvd segment:   0x01    // ex: for 1st segment
+ *      For REJECT Type:
+ *          Reject subcode: 
+ *              out of sequence:    0xFFF4
+ *              length misatch:     0xFFF5
+ *              end id missing:     0xFFF6
+ *              duplicate packet:   0xFFF7
+ *          rcvd segment:   0X01    // ex: for 1st segment
+ *      end id:         0xFFFF
+ */
+static int check_packet(char buf[]) {
+    int invalid_packet = 0x00;
+    int ack_packet = 0;
+    int reject_packet = 0;
+    int length;
+    int start = 0;
+    int i = 0;
+    int reject_subcode = 0x00;
+    int ret = 0x00;
+
+    // check header: start[0-1], client[2], type[3-4]
+    if ((unsigned char)buf[0] != 0xff && (unsigned char)buf[1] != 0xff) {
+        printf("Start of packet ID not 0xffff: buf[0]: %02x  buf[1]: %02x\n",
+            (unsigned char)buf[0], (unsigned char)buf[1]);
+        invalid_packet = 0x10;
+    }
+    if ((unsigned char)buf[2] != 0x0a) {
+        printf("Client ID not 0x0a\n: buf[2]: %02x\n", (unsigned char)buf[2]);
+        invalid_packet = 0x10;
+    }
+    if ((unsigned char)buf[3] != 0xff &&
+        ((unsigned char)buf[4] != 0xf2 || (unsigned char)buf[4] != 0xf3)) {
+        printf("Packet type not ACK nor REJECT 0xfff1: buf[3]: %02x  buf[4]: %02x\n",
+            (unsigned char)buf[3], (unsigned char)buf[4]);
+        invalid_packet = 0x10;
+    } else {
+        if ((unsigned char)buf[4] == 0xf2) {
+            printf("Packet type: ACK\n");
+            ack_packet = 1;
+            reject_subcode = 0x00;
+        }
+        if ((unsigned char)buf[4] == 0xf3) {
+            printf("Packet type: REJECT\n");
+            invalid_packet = 0x10;
+            printf("setting reject_packet after Packet type: REJECT\n");
+            reject_packet = 1;
+        }
+    }
+
+    // check ACK header: seg[5]
+    // need to detect out of sequence
+    if (ack_packet && (unsigned char)buf[5] != segment_number) {
+        printf("ACK packet segment not %02x buf[5]: %02x\n",
+            segment_number, (unsigned char)buf[5]);
+        invalid_packet = 0x10;
+    }
+
+    // need to detect out of sequence
+    // need to detect duplicate packet
+    // need to detect length mismatch
+    
+    // check end id[2]
+    if (ack_packet && (unsigned char)buf[6] != 0xff &&
+        (unsigned char)buf[6] != 0xff) {
+        printf("End of Packet Id not 0xffff: buf[6]: %02x\n",
+            (unsigned char)buf[6]);
+        invalid_packet = 0x10;
+    }
+
+    // check REJECT header: subcode[5-6]
+    if (reject_packet && ((unsigned char)buf[5] != 0xff &&
+        ((unsigned char)buf[6] < 0xf4) || (unsigned char)buf[6] > 0xf7)) {
+        printf("Invalid Reject Sub-code: buf[5]: %02x buf[6]: %02x\n",
+            (unsigned char)buf[5], (unsigned char)buf[6]);
+        invalid_packet = 0x10;
+
+    } else if (reject_packet && (unsigned char)buf[5] == 0xff &&
+        ((unsigned char)buf[6] > 0xf43) || (unsigned char)buf[6] < 0xf8) {
+        // check sub codes
+        if ((unsigned char)buf[6] == 0xf4) {
+            printf(" Reject Packet Sub-code: Out of sequence segment\n");
+            reject_subcode = 0x04;
+        }
+        if ((unsigned char)buf[6] == 0xf5) {
+            printf(" Reject Packet Sub-code: Length misatch\n");
+            reject_subcode = 0x05;
+        }
+        if ((unsigned char)buf[6] == 0xf6) {
+            printf(" Reject Packet Sub-code: End of packet missing\n");
+            reject_subcode = 0x06;
+        }
+        if ((unsigned char)buf[6] == 0xf7) {
+            printf(" Reject Packet Sub-code: Duplicate packet\n");
+            reject_subcode = 0x07;
+        }
+    }
+    ret = invalid_packet | reject_subcode;
+
+    return(ret);
+}
+
 int main(void)
 {
     struct sockaddr_in si_other;
     int s, i, slen=sizeof(si_other);
     char buf[BUFLEN];
     char message[BUFLEN];
+    char cmd[BUFLEN];
+    int ret = 0x00;
+    int start;
  
     if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
@@ -39,17 +159,48 @@ int main(void)
         exit(1);
     }
  
+    // cmd buf memory
+    memset(cmd,'\0', 1);
     while(1)
     {
-        printf("Enter message : ");
-        gets(message);
-         
-        //send the message
-        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        printf("Send message?: (y/n) ");
+        gets(cmd);
+        if (strcmp((const char *)cmd, "n") == 0) {
+		    break;
+	    } else {
+	        printf("Sending segment: %d\n", segment_number);
+	    }
+    
+        memset(message,'\0', BUFLEN);
+	    message[0] = 0xff; // packet start id
+	    message[1] = 0xff;
+	    message[2] = 0x0a; // client id
+	    message[3] = 0xff; // packet type = data
+	    message[4] = 0xf1; 
+	    message[5] = segment_number; // seg no
+	    message[6] = 0xff; // length of 264 bytes
+	    // payload of 255 bytes
+	    int i;
+	    for (i=7; i<=261;i++) {
+		    message[i] = 0xda;
+	    }
+	    message[262] = 0xff; // packet end
+	    message[263] = 0xff;
+        
+        start = 0;
+        printf("Sending packet with header: ");
+        for (i=0; i < 9; i++) {
+            printf((i<start)?"   ":"%02x%c",
+                (unsigned char)message[i],((i+1)&15)?' ':'\n');
+	    }
+	    printf("\n");
+        //send the 1st packet
+        if (sendto(s, message, strlen(message) , 0 ,
+            (struct sockaddr *) &si_other, slen)==-1)
         {
             die("sendto()");
         }
-         
+        
         //receive a reply and print it
         //clear the buffer by filling null, it might have previously received data
         memset(buf,'\0', BUFLEN);
@@ -59,7 +210,38 @@ int main(void)
             die("recvfrom()");
         }
          
-        puts(buf);
+        //puts(buf);
+        start = 0;
+        printf("Replay Packet: ");
+        for (i=0; i < 9; i++) {
+            printf((i<start)?"   ":"%02x%c", (unsigned char)buf[i],((i+1)&15)?' ':'\n');
+	    }
+	    printf("\n");
+	    
+	    // check the reply packet:
+	    //      0x1N => invalid packet
+	    //      0x0N => valid packet ack or reject.
+	    ret = check_packet(buf);
+	    if ((ret | 0x0f) == 0x1f) {
+	        printf("Invalid Replay packet\n");
+	    } else if ((ret & 0x0f) == 0x00) {
+	        printf("ACK\n");
+	    } else if ((ret & 0x0f) == 0x04) {
+	        printf("REJECT: out of sequence\n");
+	    } else if ((ret & 0x0f) == 0x05) {
+	        printf("REJECT: length mismatch");
+	    } else if ((ret & 0x0f) == 0x06) {
+	        printf("REJECT: end of packet missing\n");
+	    } else if ((ret & 0x0f) == 0x07) {
+	        printf("REJECT: duplicate packet\n");
+	    }
+
+	    // increment the segment number
+	    segment_number++;
+	    
+	    if (segment_number > 5) {
+	        break;
+	    }
     }
  
     close(s);
--- server.c
+++ server.c
@@ -8,7 +8,9 @@
 #include<arpa/inet.h>
 #include<sys/socket.h>
  
-#define BUFLEN 512  //Max length of buffer
+#define BUFLEN 264  //Max length of buffer was 512
+#define ACKBUFLEN 7
+#define REJECTBUFLEN 10
 #define PORT 8888   //The port on which to listen for incoming data
  
 void die(char *s)
@@ -16,13 +18,123 @@ void die(char *s)
     perror(s);
     exit(1);
 }
- 
+
+/*
+ * Check packet structure
+ *      start id:       0xFFFF
+ *      client id:      0x0a    // for example
+ *      Pkt types:
+ *          DATA:       0xFFF1
+ *          ACK:        0xFFF2
+ *          REJECT:     0xFFF3
+ *      For DATA Type:
+ *          sent segment:   0x01    // ex: for 1st segment
+ *          data length:    0xFF    // for 255 data bytes
+ *          payload:        0xFF bytes // using "da" for data
+ *      For ACK Type:
+ *          rcvd segment:   0x01    // ex: for 1st segment
+ *      For REJECT Type:
+ *          Reject subcode: 
+ *              out of sequence:    0xFFF4
+ *              length misatch:     0xFFF5
+ *              end id missing:     0xFFF6
+ *              duplicate packet:   0xFFF7
+ *          rcvd segment:   0X01    // ex: for 1st segment
+ *      end id:         0xFFFF
+ */
+static int check_packet(char buf[], char resp_buf[]) {
+    int data_packet = 0;
+    int length;
+    int start = 0;
+    int i = 0;
+    int reject_code = 0x00;
+
+    //for (i=0; i < BUFLEN; i++) {
+    //        printf((i<start)?"   ":"%02x%c", (unsigned char)buf[i],((i+1)&15)?' ':'\n');
+    //}
+
+    // check header: start[0-1], client[2], type[3-4]
+    if ((unsigned char)buf[0] != 0xff && (unsigned char)buf[1] != 0xff) {
+        printf("Start of packet ID not 0xffff: buf[0]: %02x  buf[1]: %02x\n",
+            (unsigned char)buf[0], (unsigned char)buf[1]);
+        data_packet= 1;
+    }
+    if ((unsigned char)buf[2] != 0x0a) {
+        printf("Client ID not 0x0a\n: buf[2]: %02x\n", (unsigned char)buf[2]);
+        data_packet = 1;
+    }
+    if ((unsigned char)buf[3] != 0xff && (unsigned char)buf[4] != 0xf1) {
+        printf("Packet type not DATA 0xfff1: buf[3]: %02x  buf[4]: %02x\n",
+            (unsigned char)buf[3], (unsigned char)buf[4]);
+        data_packet = 1;
+    } else {
+        printf("Packet type: DATA\n");
+    }
+
+    // check DATA header: seg[5], length[6], payload[length[6]]
+    // need to detect out of sequence
+    if (data_packet && (unsigned char)buf[5] != 0x01) {
+        printf("Data packet segment not 0x01: buf[5]: %02x\n",
+            (unsigned char)buf[5]);
+        data_packet = 1;
+        reject_code = 0xf4;
+    }
+
+    // need to detect out of sequence
+    // need to detect duplicate packet
+    // need to detect length mismatch
+    
+    // check end id[2]
+    length = buf[6];
+    if (data_packet && (unsigned char)buf[6 + length] != 0xff &&
+        (unsigned char)buf[6 + length + 1] != 0xff) {
+        printf("End of Packet Id not 0xffff: buf[6]: %02x buf[6 + %d + 1]: %02x\n",
+            (unsigned char)buf[6], length, (unsigned char)buf[6 + length + 1]);
+        data_packet = 1;
+        reject_code = 0xf6;
+    }
+
+    // check ACK header: seg[1]
+    
+    // check REJECT header: subcode[2]
+
+    // compose resp_buf
+    if (data_packet == 0) {
+        // ACK packet
+        resp_buf[0] = 0xff;     // packet start id
+	    resp_buf[1] = 0xff;
+	    resp_buf[2] = buf[2];   // client id
+	    resp_buf[3] = 0xff;     // packet type: ACK
+	    resp_buf[4] = 0xf2; 
+	    resp_buf[5] = buf[5];   // seg no
+	    resp_buf[6] = 0xff;     // packet end
+	    resp_buf[7] = 0xff;
+	    resp_buf[8] = 0x00;     // pad
+    } else {
+        // REJECT packet
+        resp_buf[0] = 0xff;     // packet start id
+	    resp_buf[1] = 0xff;
+	    resp_buf[2] = buf[2];   // client id
+	    resp_buf[3] = 0xff;     // packet type:  REJECT
+	    resp_buf[4] = 0xf3; 
+	    resp_buf[5] = 0xff;     // reject subcode
+	    resp_buf[6] = reject_code;
+	    resp_buf[7] = buf[5];   // segment
+	    resp_buf[8] = 0xff;     // packet end
+	    resp_buf[9] = 0xff;     // pad
+    }
+
+    return(data_packet);
+}
+
 int main(void)
 {
     struct sockaddr_in si_me, si_other;
      
     int s, i, slen = sizeof(si_other) , recv_len;
     char buf[BUFLEN];
+    char resp_buf[REJECTBUFLEN];
+    int start = 0;
      
     //create a UDP socket
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
@@ -46,10 +158,11 @@ int main(void)
     //keep listening for data
     while(1)
     {
-        printf("Waiting for data...");
+        printf("\nWaiting for data...");
         fflush(stdout);
          
         //try to receive some data, this is a blocking call
+        memset(buf,'\0', BUFLEN);
         if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
         {
             die("recvfrom()");
@@ -57,13 +170,35 @@ int main(void)
          
         //print details of the client/peer and the data received
         printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n" , buf);
-         
-        //now reply the client with the same data
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
+        //printf("Data: %s\n" , buf);
+        
+        start = 0;
+        printf("Packet Header: ");
+        for (i=0; i < 7; i++) {
+            printf((i<start)?"   ":"%02x%c", (unsigned char)buf[i],((i+1)&15)?' ':'\n');
+	    }
+	    printf("\n");
+	    
+	    // call a packet inspector
+	    if ((check_packet(buf, resp_buf)) != 0) {
+	        printf("check_packet did not detect data packet. sending REJECT\n");
+	    } else {
+	        printf("check_packet detected data packet. sending ACK\n");
+	    }
+
+        start = 0;
+        printf("ACK Packet: ");
+        for (i=0; i < 7; i++) {
+            printf((i<start)?"   ":"%02x%c", (unsigned char)resp_buf[i],((i+1)&15)?' ':'\n');
+	    }
+	    printf("\n");
+	    
+        //now reply to the client with the resp_buf (ACK or REJECT)
+        if (sendto(s, resp_buf, REJECTBUFLEN, 0, (struct sockaddr*) &si_other, slen) == -1)
         {
             die("sendto()");
         }
+
     }
  
     close(s);
